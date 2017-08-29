0b767a34341175a37fe6df229c5d6cb7eeb205e0
cpalomino <laignelet2@gmail.com>
2017-08-28 23:03:09 -0700

    Video search added

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -14,7 +14,7 @@ const VideoDetail = ({video})=>{
 
 
     return (
-    <div className="vide-detail col-md-8">
+    <div className="video-detail col-md-8">
         <div className="embed-responsive embed-responsive-16by9">
             <iframe src={url} frameBorder="0" className="embed-responsive-item"></iframe>
         </div>
